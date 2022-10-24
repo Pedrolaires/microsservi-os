55337f43a465e620761eb10780631857da7711f1
Pedro Aires <pedro.santos.1212436@sga.pucminas.br>
2022-10-24 16:48:07 -0300

    hr-worker

---
+++ Microsservicos/hr-worker/src/test/java/com/pedrolairess/hrworker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrolairess.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
